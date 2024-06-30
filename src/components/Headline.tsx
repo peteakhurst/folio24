e4ee03dbfba799ffdb4bc53da90b8d6ffc6ef71f
peteakhurst <peteakhurst@gmail.com>
2024-06-30 15:11:21 +1000

    added text animations fixes

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,15 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import 'splitting/dist/splitting.css';
-import 'splitting/dist/splitting-cells.css';
-import Splitting from 'splitting';
 
 const Headline = () => {
-  Splitting();
-
   return (
     <div className='container '>
-      <div data-splitting className='flex justify-center py-10 uppercase'>
+      <div className='flex justify-center py-10 uppercase'>
         <h1 className='text-5xl font-black md:leading-none md:text-[11rem] font-source text-coral'>
           Pete <span className='italic font-light'>Akhurst</span>
         </h1>
