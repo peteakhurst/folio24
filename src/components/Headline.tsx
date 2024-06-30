111b06883b0a63ddc27b8433ca7765f2860aeb64
peteakhurst <peteakhurst@gmail.com>
2024-06-30 15:03:26 +1000

    added text animations fixes

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import 'splitting/dist/splitting.css';
 import 'splitting/dist/splitting-cells.css';
 import Splitting from 'splitting';
